b00938224a2950b9d3b7ac0d760106c6ae6d3e5f
Vishal <Vishal@ADMIN-PC>
2025-04-07 16:33:33 +0530

    Add Test Controller

---
+++ DemoRepo2025/Controllers/TestController1cs.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoRepo2025.Controllers
+{
+    public class TestController1cs : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
