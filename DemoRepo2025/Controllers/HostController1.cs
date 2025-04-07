084204e35e2b88a51b370c09f225a44bfddd118c
Vishal <Vishal@ADMIN-PC>
2025-04-07 16:41:46 +0530

    Add Host Controller

---
+++ DemoRepo2025/Controllers/HostController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoRepo2025.Controllers
+{
+    public class HostController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
